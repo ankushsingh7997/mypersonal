53661ae5c59060d00bb127cad27d377bc400e91e
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-01-22 17:50:57 +0530

    fjk

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -12,11 +12,13 @@ exports.genrateShortUrl = async function (req, res) {
         await axios.get(originalUrl).then(() => { isValid = true }).catch(() => { isValid = false });
         if (isValid == false) return res.status(400).send({ status: false, message: "Please provide valid url" });
         const catchedData = await redis.GET(`${originalUrl}`);
+        console.log("1")
         if(catchedData) return res.status(200).send({ status: true, message: "Url already genrated.", data: JSON.parse(catchedData) });
         const data = await urlModel.findOne({ longUrl: originalUrl }).select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
         if (data){
             res.status(200).send({ status: true, message: "Url already genrated.", data: data });
-            return await redis.SET(`${data.longUrl}`,24*60*60,JSON.stringify(data));
+            console.log("2")
+            return await redis.SET(`${data.longUrl}`,24,JSON.stringify(data));
         }
         let urlCode = shortId.generate();
         let obj = {
@@ -26,7 +28,8 @@ exports.genrateShortUrl = async function (req, res) {
         }
         await urlModel.create(obj);
         res.status(201).send({ status: true, message: "Url genrated.", data: obj });
-        await redis.SET(`${obj.longUrl}`,24*60*60,JSON.stringify(obj));
+        console.log("3")
+        await redis.SET(`${obj.longUrl}`,24,JSON.stringify(obj));
     } catch (error) {
         res.status(500).send({ status: false, message: error.message });
     }
@@ -41,8 +44,10 @@ exports.getUrl = async function (req, res) {
         const originalUrl = await urlModel.findOne({ urlCode: urlCode.toLowerCase() }).select({ _id: 0, longUrl: 1 });
         if (!originalUrl) return res.status(404).send({ status: false, message: "url not found." });
         res.redirect(302, originalUrl.longUrl);
-        await redis.SET(`${urlCode}`,24*60*60,`${originalUrl.longUrl}`);
+        await redis.SET(`${urlCode}`,24,`${originalUrl.longUrl}`);
     } catch (error) {
         res.status(500).send({ status: false, message: error.message });
     }
 }
+
+
